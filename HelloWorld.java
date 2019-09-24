b73091a1c6ee820e5b1446cfc45190524ddb4a8a
chiquillo7777 <carlosjosechiquillolopez@gmail.com>
2019-09-24 21:56:36 +0000

    a few java coding tests

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,24 +2,48 @@ import java.util.Scanner;
 
 public class HelloWorld {
 
+    public static boolean isInteger(String x){
+        try {
+            Integer.parseInt(x);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+
          Scanner sc = new Scanner (System.in);
 
-         System.out.println("Enter a ramdom Integer number between 1900 and 2016");
+         /*System.out.println("Enter a ramdom Integer number between 1900 and 2016");
          int givenNumber = sc.nextInt();
          System.out.println("\n");
 
          System.out.println("how old were you back in 2016?");
          int ageIn2016 = sc.nextInt();
+         int dateOfbirth = 2016-ageIn2016;
          System.out.println("\n");
-            if(givenNumber > ageIn2016){
+            if(givenNumber > dateOfbirth){
                 System.out.println(1);
-            }else if(givenNumber == ageIn2016 ){
+            }else if(givenNumber == dateOfbirth ){
                 System.out.println(0);
-            }else if (givenNumber < ageIn2016){
+            }else if (givenNumber < da){
                  System.out.println(-1);
+            }*/
+
+        String array1[] = {"1", "6","6", "h","h", "9","7", "5"};
+        int sum=0;
+        for (int i=0; i < array1.length; i++) {
+            try {
+                 if(i%2 == 0 && isInteger(array1[i]) ){
+                        sum = sum + Integer.parseInt(array1[i]);
+                }
+            } catch (Exception e) {
+                throw e;
             }
 
+        }
+        System.out.println(sum);
     }
 }
